bf5bbcef6634b8930229d389a048e4bcc03efcfb
Nicolas Schmitz <nicolas.b.schmitz@gmail.com>
2023-01-09 15:28:13 -0300

    Innitial commit

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private Rigidbody2D birdRigidbody;
+    [SerializeField]
+    private float flapStrength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            birdRigidbody.velocity = Vector2.up * flapStrength;
+        }
+    }
+}
